949c615efe7ae763dad3a5cefcbce50c90e29aca
Laurent Tréguier <laurent.treguier@with-madrid.com>
2023-07-04 18:07:49 +0200

    Delete remote files held by renders

--- src/wideo/signals.py
+++ src/wideo/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_delete, post_save, pre_delete, pre_save
 from django.dispatch import receiver
 
-from . import get_video_model
-from .models import AbstractVideo, UploadedVideo
+from . import get_render_model, get_video_model
+from .models import AbstractRender, AbstractVideo, UploadedVideo
 from .tasks import encode_video, is_celery_present
 
 
@@ -34,3 +34,8 @@ def on_video_post_save(instance: AbstractVideo, created: bool, *args, **kwargs):
     """
     if created and is_celery_present():
         encode_video.delay(video_id=instance.id)
+
+
+@receiver(pre_delete, sender=get_render_model())
+def on_render_pre_delete(instance: AbstractRender, *args, **kwargs):
+    instance.file.delete(save=False)
